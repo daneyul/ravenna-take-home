f9a5bb34cac59a3a6b84b30c965b07835f00dceb
daneyul <dan.nguyen5@gmail.com>
2025-12-11 11:13:52 -0800

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { Provider } from "jotai";
 import { Toaster } from "sonner";
 import { Sidebar } from "@/components/Sidebar";
@@ -22,6 +23,7 @@ export default function RootLayout({
             <Sidebar />
             <main className="flex-1 flex flex-col overflow-hidden my-2 mr-2 ml-2 md:ml-0 bg-stone-50/90 rounded-md border border-stone-200">
               {children}
+              <Analytics />
             </main>
           </div>
           <Toaster position="bottom-right" />
